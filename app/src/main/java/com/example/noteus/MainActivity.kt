8d9c45390bb0a4d201a00759a938f4a1c51055b4
Randil22 <thenularandila2002@gmail.com>
2024-10-11 03:15:56 +0530

    done

--- app/src/main/java/com/example/noteus/MainActivity.kt
+++ app/src/main/java/com/example/noteus/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.noteus
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.noteus.database.NoteDatabase
+import com.example.noteus.repository.NoteRepository
+import com.example.noteus.viewmodel.NoteViewModeFactory
+import com.example.noteus.viewmodel.NoteViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModeFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
